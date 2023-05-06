3e7d9b761aa6eeb770c3d2b4c56829e4f5f9cb7b
sharonjung08 <132734324+sharonjung08@users.noreply.github.com>
2023-05-06 14:48:10 +0900

    Update 2강.R

--- 2강.R
+++ 2강.R
@@ -1,7 +1,7 @@
 
 ### R Working Directory
 getwd()
-setwd("C:\\Users\\socra\\Downloads\\")
+setwd("C:\\Users\\everu\\Downloads\\r studio works\\")
 
 data<-read.csv("건강보험심사평가원_의료행위별 성별 연령군별 건강보험 진료 통계_20141231.csv", 
   header=TRUE, fileEncoding="euc-kr")
@@ -64,7 +64,6 @@ rename(data, year = 진료년도, sex = 성별)
 library(dplyr)
 new_data<-rename(data, year = 진료년도, sex = 성별)
 summary(new_data)
-
 
 install.packages('glmnet')
 library(glmnet)
@@ -219,7 +218,7 @@ h<-function(KOR, JAP) {
   }
 }
 
-a<-c(1:5)
+a<-c(1:5) ##from 1~5 (1:10 = from 1~10)
 b<-c(0, 2, 4, 2, 1)
 
 h(a, b)
